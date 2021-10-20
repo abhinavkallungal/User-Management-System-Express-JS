e4a7bf94cf7ec8affdcb5cf0e8430656bdfdc6a3
Abhinav Kallungal <abhinavkallungal15@gmail.com>
2021-10-20 18:51:13 +0530

    user side design Completed home page ,login page ,signup page

--- helpers/userHelpers.js
+++ helpers/userHelpers.js
@@ -2,5 +2,11 @@ const db= require('../config/connection');
 const collections= require('../config/collections')
 
 module.exports={
+    checkEmail:()=>{
 
+    },
+    signUp:(data)=>{
+        console.log(data);
+        
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const userHelpers= require('../helpers/userHelpers')
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -7,6 +8,17 @@ router.get('/', function(req, res) {
 });
 router.get('/login',(req,res)=>{
 res.render('user/userLogin');
+});
+
+router.get('/signup',(req,res)=>{
+  res.render('user/userSignup');
+});
+router.post('/signup',(req,res)=>{
+  userHelpers.signUp(req.body)
+})
+
+router.post('/checkemail',(req,res)=>{
+
 })
 
 module.exports = router;
