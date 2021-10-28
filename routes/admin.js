38b5ede32d16716449c9aa1668cf502b9419adf8
Abhinav Kallungal <abhinavkallungal15@gmail.com>
2021-10-28 10:43:25 +0530

    alert actions are  added

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,20 @@
-var ObjectId=require('mongodb').ObjectID
+var ObjectId = require('mongodb').ObjectID
 var express = require('express');
 const adminHelpers = require('../helpers/adminHelpers');
 var router = express.Router();
-const userHelpers=require('../helpers/userHelpers');
+const userHelpers = require('../helpers/userHelpers');
 
 
 const verifyLogin = (req, res, next) => {
-  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');  
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   if (req.session.admin) {
-   next()
+    next()
   } else {
     res.redirect("/admin/login");
   }
 };
 
-const checkSession= (req, res, next) => {
+const checkSession = (req, res, next) => {
   if (req.session.admin) {
     res.redirect("/admin");
   } else {
@@ -25,147 +25,165 @@ const checkSession= (req, res, next) => {
 
 
 /* GET home page. */
-router.get('/', verifyLogin, function(req, res){
-  
-  userHelpers.getUsers().then((users)=>{
-    res.render('admin/adminHome',{admin:true,users,title:" Admin  Dashboard"});
-  }).catch((message)=>{
-    res.render('admin/adminHome',{admin:true,message,title:" Admin  Dashboard"});
+router.get('/', verifyLogin, function (req, res) {
+
+  userHelpers.getUsers().then((users) => {
+    res.render('admin/adminHome', { admin: true, users, title: " Admin  Dashboard" });
+  }).catch((message) => {
+    res.render('admin/adminHome', { admin: true, message, title: " Admin  Dashboard" });
   })
 });
 
 
 
-router.get('/login',checkSession,function(req,res){
-  res.render("admin/adminLogin",{title:" Admin  Login"})
+router.get('/login', checkSession, function (req, res) {
+  res.render("admin/adminLogin", { title: " Admin  Login" })
 });
 
-router.post('/login',checkSession,(req,res)=>{
-  adminHelpers.login(req.body).then((response)=>{
-    req.session.admin=response.user;
-    res.json({logedIn:true})
-  }).catch((err)=>{
+router.post('/login', checkSession, (req, res) => {
+  adminHelpers.login(req.body).then((response) => {
+    req.session.admin = response.user;
+    res.json({ logedIn: true })
+  }).catch((err) => {
     res.json(err)
   })
 });
 
-router.get('/deleteUser/:id',verifyLogin,(req,res)=>{
-    let id = req.params.id
-    userHelpers.deleteUser(id).then(()=>{
-      res.redirect('/admin/')
-    })
+router.post('/deleteUser/', verifyLogin, (req, res) => {
+  let id = req.body.id
+  userHelpers.deleteUser(id).then(() => {
+    res.redirect('/admin/')
+  })
 });
 
-router.get('/editUser/:id',verifyLogin,(req,res)=>{
+router.get('/editUser/:id', verifyLogin, (req, res) => {
   let id = req.params.id
-  userHelpers.getUser(id).then((userDetails)=>{
-    res.render('admin/editUser',{admin:true,userDetails,title:" Edit User"});
-  }).catch(()=>{
+  userHelpers.getUser(id).then((userDetails) => {
+    res.render('admin/editUser', { admin: true, userDetails, title: " Edit User" });
+  }).catch(() => {
     res.redirect("/admin/");
   })
-  
+
 });
 
-router.post('/editUser/',verifyLogin, async(req,res)=>{
+router.post('/editUser/', verifyLogin, async (req, res) => {
+  console.log("Dfasdfas");
+  let emailCheck = false;
+  let emailExistCheck = false;
 
-  let emailCheck =false;
-  let emailExistCheck =false;
+  await userHelpers.checkEmail(req.body.email).then(() => {
+    emailCheck = true
+    console.log("Dfasdfas2");
 
-  await userHelpers.checkEmail(req.body.email).then(()=>{
-    emailCheck=true
-  }).catch((err)=>{ 
-    emailCheck=false;
+  }).catch((err) => {
+    emailCheck = false;
+    console.log("Dfasdfas3");
+
     res.json(err);
   });
+  console.log("Dfasdfas4");
 
-  await userHelpers.checkEmailExist(req.body.email).then((response)=>{
 
-    emailExistCheck=true
+  await userHelpers.checkEmailExist(req.body.email).then((response) => {
 
-  }).catch((err)=>{
+    emailExistCheck = true
+    console.log("Dfasdfas5");
 
-    if(req.body.id === err.id.toString()){
-      emailExistCheck=true
+  }).catch((err) => {
+    console.log("Dfasdfas6");
 
-    }else{
-      emailExistCheck=false
+    if (req.body.id === err.id.toString()) {
+      emailExistCheck = true
+      console.log("Dfasdfas7");
+
+    } else {
+      console.log("Dfasdfas8");
+      emailExistCheck = false
       res.json({
-        message:'Email already Exist',
-        error:true
+        message: 'Email already Exist',
+        error: true
+      });
+
+
+    }
+
+  })
+  console.log("Dfasdfas9");
+
+
+  if (emailExistCheck === true && emailCheck === true) {
+    console.log("Dfasdfas10");
+    userHelpers.editUser(req.body).then(() => {
+      console.log("Dfasdfas11");
+
+      res.json({ edited: true })
+    }).catch((err) => {
+      console.log("Dfasdfas12");
+      console.log(err);
+    })
+  } else {
+    console.log("Dfasdfas13");
+    res.json({
+      message: 'Editing have a Problum',
+      error: true
     });
 
 
-    }
-
-  })
-
-  if(emailExistCheck === true &&  emailCheck === true){
-    userHelpers.editUser(req.body).then(()=>{
-      res.json({edited:true})
-    }).catch((err)=>{
-    })
-  }else{
-    res.json({
-      message:'Editing have a Problum',
-      error:true
-  });
-
-
   }
-   
+
 });
 
 
-router.get('/addUser',verifyLogin,(req,res)=>{
-  res.render('admin/addUser',{admin:true,title:"Add User"})
+router.get('/addUser', verifyLogin, (req, res) => {
+  res.render('admin/addUser', { admin: true, title: "Add User" })
 });
 
-router.post('/addUser',verifyLogin,async(req,res)=>{
-  let result={}
-  email=req.body.email;
+router.post('/addUser', verifyLogin, async (req, res) => {
+  let result = {}
+  email = req.body.email;
 
-  await userHelpers.checkEmailExist(email).then((response)=>{
-    result.emailExist=response;
-  }).catch((err)=>{
-    result.emailExist=err;
+  await userHelpers.checkEmailExist(email).then((response) => {
+    result.emailExist = response;
+  }).catch((err) => {
+    result.emailExist = err;
   })
 
 
-  await userHelpers.checkEmail(email).then((response)=>{
-    result.emailVerification=response;
-  }).catch((err)=>{
-    result.emailVerification=err;
+  await userHelpers.checkEmail(email).then((response) => {
+    result.emailVerification = response;
+  }).catch((err) => {
+    result.emailVerification = err;
   })
 
-  result.passwordVerification=false;
-  password=req.body.password;
- await userHelpers.checkPassword(password).then((response)=>{
-    result.passwordVerification=response;
-  }).catch((err)=>{
-     result.passwordVerification=err;
+  result.passwordVerification = false;
+  password = req.body.password;
+  await userHelpers.checkPassword(password).then((response) => {
+    result.passwordVerification = response;
+  }).catch((err) => {
+    result.passwordVerification = err;
   })
 
 
-  result.confirmPasswordVerification=false;
-  if(result.passwordVerification){
-    if(req.body.password===req.body.confirmPassword){
-      result.confirmPasswordVerification={error:false};
-    }else{
-      result.confirmPasswordVerification={error:true,message:"The password and confirmation password do not match "};
+  result.confirmPasswordVerification = false;
+  if (result.passwordVerification) {
+    if (req.body.password === req.body.confirmPassword) {
+      result.confirmPasswordVerification = { error: false };
+    } else {
+      result.confirmPasswordVerification = { error: true, message: "The password and confirmation password do not match " };
     }
   }
-  
-  if(result.emailVerification.error===false && result.passwordVerification.error===false && result.confirmPasswordVerification.error===false && result.emailExist.error===false){
-    userHelpers.signUp(req.body).then((response)=>{
-      result.logedIn=response
-      req.body.status="Active";
-      req.session.admin=req.body;
+
+  if (result.emailVerification.error === false && result.passwordVerification.error === false && result.confirmPasswordVerification.error === false && result.emailExist.error === false) {
+    userHelpers.signUp(req.body).then((response) => {
+      result.logedIn = response
+      req.body.status = "Active";
+      req.session.admin = req.body;
       res.json(result)
-    }).catch((err)=>{
-      result.logedIn=false
+    }).catch((err) => {
+      result.logedIn = false
       res.json(result)
     })
-  }else{
+  } else {
     res.json(result)
 
   }
@@ -173,9 +191,9 @@ router.post('/addUser',verifyLogin,async(req,res)=>{
 });
 
 
-router.get('/logout',(req,res)=>{
-  req.session.admin=null
-  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');  
+router.get('/logout', (req, res) => {
+  req.session.admin = null
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.redirect('/admin/login');
 })
 
